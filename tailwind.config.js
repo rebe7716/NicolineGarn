8209af362e6724cd6adf7b81a75f3e070be4edc6
rebe7716 <rebe7716@stud.kea.dk>
2022-11-28 19:02:10 +0100

    lots

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      /*beige*/
+      beige: "#FFF7EF",
+      /*lyspink*/
+      lyspink: "#FFF5F5",
+      /*pink*/
+      pink: "#EED6D6",
+      /*rød*/
+      rød: "#FF4D00",
+      /*sort*/
+      sort: "#000000",
+      /*hvid*/
+      hvid: "#FFFFFF",
+    },
+
+    fontFamily: {
+      Heebo: "'Heebo', sans-serif;",
+      TitilliumRegular: "'TitilliumRegular', sans-serif",
+      TitilliumLight: "'TitilliumLight', sans-serif", 
+
+    },
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
-};
+  plugins: [],
+}
