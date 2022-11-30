cab469b98214974a865fa160058d7896ed4af7e3
rebe7716 <rebe7716@stud.kea.dk>
2022-11-30 14:48:15 +0100

    hej

---
+++ script/mixitupinit.js
@@ -0,0 +1,1 @@
+var mixer = mixitup('.forside_filtrering_div');
---
+++ script/productgallery.js
@@ -0,0 +1,27 @@
+/* product left start */
+if ($(".product-left").length) {
+  var productSlider = new Swiper(".product-slider", {
+    spaceBetween: 0,
+    centeredSlides: false,
+    loop: true,
+    direction: "horizontal",
+    loopedSlides: 5,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    resizeObserver: true,
+  });
+  var productThumbs = new Swiper(".product-thumbs", {
+    spaceBetween: 0,
+    centeredSlides: true,
+    loop: true,
+    slideToClickedSlide: true,
+    direction: "horizontal",
+    slidesPerView: 5,
+    loopedSlides: 5,
+  });
+  productSlider.controller.control = productThumbs;
+  productThumbs.controller.control = productSlider;
+}
+/* 	product left end */
